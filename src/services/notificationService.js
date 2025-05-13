708b1dd49b930a15bd04df534272822cd6734956
abrakazoo <avishavit101@gmail.com>
2025-05-13 18:29:39 +0300

    Add notification service logic

--- src/services/notificationService.js
+++ src/services/notificationService.js
@@ -1,18 +1,59 @@
 const axios = require('axios');
+const http = require('http');
+const instance = axios.create({
+  httpAgent: new http.Agent({ keepAlive: false}),
+});
 
 const notificationHandlers = {
   email: async (user, message) => {
-    return await axios.post('http://localhost:5001/send-email', {
+    let data = JSON.stringify({
       email: user.email,
       message,
     });
+    let config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'http://localhost:5001/send-email',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: data,
+    };
+    const response = await instance(config);
+
+    if (response.status !== 200) {
+      throw new Error('Bad response from email service');
+    }
+    console.log('Email sent successfully:', JSON.stringify(response.data));
+
+    return response;
+
   },
   sms: async (user, message) => {
-    return await axios.post('http://localhost:5001/send-sms', {
+    let data = JSON.stringify({
       telephone: user.telephone,
       message,
     });
-  },
+
+    let config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'http://localhost:5001/send-sms',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: data,
+    };
+
+    const response = await instance(config);
+
+    if (response.status !== 200) {
+      throw new Error('Bad response from email service');
+    }
+    console.log('SMS sent successfully:', JSON.stringify(response.data));
+
+    return response;
+  }
 };
 
 exports.sendNotifications = async (user, message) => {
